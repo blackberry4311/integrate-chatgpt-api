8ec68de98c7ae7865485cd4588ac1c1e5cfb906a
blackberry4311 <sytoanit@gmail.com>
2024-03-24 17:12:56 +0700

    fix import telegram bot

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import OpenAI from 'openai';
 import { ConfigService } from '@nestjs/config';
-import * as TelegramBot from 'node-telegram-bot-api';
+import TelegramBot from 'node-telegram-bot-api';
 import { TelegramService } from './telegram.service';
 
 @Injectable()
