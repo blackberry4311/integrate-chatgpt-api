0006b4fb2918d78f0e6e920d63625fd101970cc2
blackberry4311 <sytoanit@gmail.com>
2024-03-24 16:00:42 +0700

    - add function to integrate with assistants APIs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -7,6 +7,6 @@ import { TelegramService } from './telegram.service';
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, TelegramService],
+  providers: [AppService, TelegramService, Logger],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import OpenAI from 'openai';
 import { ConfigService } from '@nestjs/config';
 import * as TelegramBot from 'node-telegram-bot-api';
@@ -6,8 +6,13 @@ import { TelegramService } from './telegram.service';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   private readonly openai: any;
   private readonly assistantId: string;
+
+  //need to store these thread IDs to db to retrieve later on
+  private readonly threadIDs: Map<number, any>;
+
   constructor(
     private readonly configService: ConfigService,
     private readonly telegramService: TelegramService,
@@ -19,11 +24,30 @@ export class AppService {
       this.handleIncomingMessage.bind(this),
     );
     this.assistantId = this.configService.get<string>('ASSISTANT_ID');
+    this.threadIDs = new Map();
   }
 
-  private _processOpenAIRes(res: any) {
-    return '';
-  }
+  _getOrCreateThread = async (localId: number) => {
+    let existed = this.threadIDs.get(localId);
+    if (!existed) {
+      existed = await this.openai.beta.threads.create();
+      this.threadIDs.set(localId, existed);
+    }
+    return existed;
+  };
+
+  _appendMesToThread = async (threadId: string, message: string) => {
+    await this.openai.beta.threads.messages.create(threadId, {
+      role: 'user',
+      content: message,
+    });
+  };
+
+  _createRun = async (threadId: string) => {
+    return this.openai.beta.threads.runs.create(threadId, {
+      assistant_id: this.assistantId,
+    });
+  };
 
   async chatCompletions(message: string): Promise<{ message: string }> {
     const chat = await this.openai.chat.completions.create({
@@ -34,19 +58,59 @@ export class AppService {
     return { message: choices[0].message.content };
   }
 
-  // async assistantSupport(message: string): Promise<{ message: string }> {
-  //   const params = {
-  //     engine: this.assistantId, // Specify the engine (assistant) you want to use
-  //     prompt: message,
-  //   };
-  //   const res = await this.openai.beta.threads.cre
-  //   return { message: res.data.choices[0].text.trim() };
-  // }
+  async assistantSupport(message: TelegramBot.Message): Promise<void> {
+    const {
+      from: { id },
+      chat: { id: replyId },
+      text,
+    } = message;
+    const currentThread = await this._getOrCreateThread(id);
+    await this._appendMesToThread(currentThread.id, text);
+    const myRun = await this._createRun(currentThread.id);
+    const retrieveRun = async () => {
+      let keepRetrievingRun;
+
+      while (myRun.status === 'queued' || myRun.status === 'in_progress') {
+        keepRetrievingRun = await this.openai.beta.threads.runs.retrieve(
+          currentThread.id,
+          myRun.id,
+        );
+        this.logger.debug(`Run status: ${keepRetrievingRun.status}`);
+
+        if (keepRetrievingRun.status === 'completed') {
+          // Step 6: Retrieve the Messages added by the Assistant to the Thread
+          const allMessages = await this.openai.beta.threads.messages.list(
+            currentThread.id,
+          );
+
+          this.logger.debug(
+            'Assistant: ',
+            allMessages.data[0].content[0].text.value,
+          );
+          this.telegramService.sendMessage(
+            replyId,
+            allMessages.data[0].content[0].text.value,
+          );
+
+          break;
+        } else if (
+          keepRetrievingRun.status === 'queued' ||
+          keepRetrievingRun.status === 'in_progress'
+        ) {
+          // pass
+        } else {
+          this.logger.log(`Run status: ${keepRetrievingRun.status}`);
+          break;
+        }
+      }
+    };
+    retrieveRun();
+  }
 
   async handleIncomingMessage(message: TelegramBot.Message) {
     // Handle incoming message here
-    console.log(message.text);
-    const reply = await this.chatCompletions(message.text);
-    this.telegramService.sendMessage(message.chat.id, reply.message);
+    this.logger.debug(message.text);
+    await this.assistantSupport(message);
+    // this.telegramService.sendMessage(message.chat.id, reply.message);
   }
 }
